55fa37c23734bcadeafcbb09463604911cc1febf
Asrath <pasrath.2004@gmail.com>
2025-07-17 19:19:54 +0530

    added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromChildren,
+  Route,
+} from 'react-router-dom';
+import Home from './pages/Home.tsx';
+import SignIn from './pages/SignIn.tsx';
 
+const router = createBrowserRouter(
+  createRoutesFromChildren(
+    <>
+      <Route path='/' element={<Home />} />
+      <Route path='/sign-in' element={<SignIn />} />
+    </>
+  )
+);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
